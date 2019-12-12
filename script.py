635c6b7ab405eac0000360f17ef74deb27a1a5fb
Christopher Lamm <chris@theantichris.com>
2019-12-12 07:20:18 -0500

    added remove_child() method

--- script.py
+++ script.py
@@ -5,3 +5,10 @@ class TreeNode:
 
     def add_child(self, child_node):
         self.children.append(child_node)
+
+    def remove_child(self, child_node):
+        new_children = []
+        for child in self.children:
+            if child.value != child_node.value:
+                new_children.append(child)
+        self.children = new_children
