9831bfe1aa4ce9f020498744c4e40fc632e9e463
Christopher Lamm <chris@theantichris.com>
2019-12-12 07:36:06 -0500

    fixed traverse() to go through all child nodes

--- script.py
+++ script.py
@@ -13,16 +13,20 @@ class TreeNode:
         self.children = [child for child in self.children if child is not child_node]
 
     def traverse(self):
-        print(self)
-        for child in self.children:
-            print(child)
+        nodes_to_visit = [self]
+        while len(nodes_to_visit) > 0:
+            current_node = nodes_to_visit.pop()
+            print(current_node)
+            nodes_to_visit += current_node.children
 
 
 root = TreeNode("CEO")
 first_child = TreeNode("Vice-President")
 second_child = TreeNode("Head of Marketing")
+third_child = TreeNode("Marketing Assistant")
 
 root.add_child(first_child)
 root.add_child(second_child)
+second_child.add_child(third_child)
 
 root.traverse()
