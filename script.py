2e4b2fa1b186a0234a8f6f871113430d2c089fbd
Christopher Lamm <chris@theantichris.com>
2019-12-12 07:16:24 -0500

    adding a child

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
 class TreeNode:
     def __init__(self, value):
         self.value = value
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
