5b83ecc866f40c03d48e3effac636257fd4e95da
Christopher Lamm <chris@theantichris.com>
2019-12-12 07:28:32 -0500

    added traverse() method

--- script.py
+++ script.py
@@ -3,8 +3,26 @@ class TreeNode:
         self.value = value
         self.children = []
 
+    def __repr__(self):
+        return str(self.value)
+
     def add_child(self, child_node):
         self.children.append(child_node)
 
     def remove_child(self, child_node):
-        self.children = [child for child in self.children if child.value != child_node.value]
+        self.children = [child for child in self.children if child is not child_node]
+
+    def traverse(self):
+        print(self)
+        for child in self.children:
+            print(child)
+
+
+root = TreeNode("CEO")
+first_child = TreeNode("Vice-President")
+second_child = TreeNode("Head of Marketing")
+
+root.add_child(first_child)
+root.add_child(second_child)
+
+root.traverse()
